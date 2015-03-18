6aa0a836749c851edc068cec8a94277850b01133
Daniel Holzmann <d@velopment.at>
2015-03-18 14:20:32 +0100

    fixed "Type cl_encrypted already exists." exception on cache clear+warmup

--- CodeLoversDoctrineEncryptBundle.php
+++ CodeLoversDoctrineEncryptBundle.php
@@ -6,37 +6,37 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CodeLoversDoctrineEncryptBundle extends Bundle
 {
-    private $hasMongoDb = false;
-    private $hasORM = false;
+    private static $hasMongoDb = false;
+    private static $hasORM = false;
     public function __construct()
     {
-        if (class_exists('Doctrine\ODM\MongoDB\Types\Type')) {
+        if (false === static::$hasMongoDb && class_exists('Doctrine\ODM\MongoDB\Types\Type')) {
             \Doctrine\ODM\MongoDB\Types\Type::registerType(TypeName::ENCRYPTED_TYPE_NAME, 'CodeLovers\DoctrineEncryptBundle\MongoDB\Type\EncryptedType');
-            $this->hasMongoDb = true;
+            static::$hasMongoDb = true;
         }
 
-        if (class_exists('Doctrine\DBAL\Types\Type')) {
+        if (false === static::$hasORM && class_exists('Doctrine\DBAL\Types\Type')) {
             \Doctrine\DBAL\Types\Type::addType(TypeName::ENCRYPTED_TYPE_NAME, 'CodeLovers\DoctrineEncryptBundle\ORM\Type\EncryptedType');
-            $this->hasORM = true;
+            static::$hasORM = true;
         }
     }
 
     public function boot()
     {
-        if (false === $this->hasORM && false === $this->hasMongoDb) {
+        if (false === static::$hasORM && false === static::$hasMongoDb) {
             return;
         }
 
         $encryptor = $this->container->get('code_lovers.encryptor');
 
-        if (true === $this->hasMongoDb) {
+        if (true === static::$hasMongoDb) {
             /** @var MongoDB\Type\EncryptedType $type */
             $type = \Doctrine\ODM\MongoDB\Types\Type::getType(TypeName::ENCRYPTED_TYPE_NAME);
             $type->setEncryptor($encryptor)
                 ->setSecret($this->container->getParameter('code_lovers.encryptor.secret'));
         }
 
-        if (true === $this->hasORM) {
+        if (true === static::$hasORM) {
             /** @var ORM\Type\EncryptedType $type */
             $type = \Doctrine\DBAL\Types\Type::getType(TypeName::ENCRYPTED_TYPE_NAME);
             $type->setEncryptor($encryptor)
